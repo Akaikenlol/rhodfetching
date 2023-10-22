d0c10d0888cb71f5d5e1695025f4bbe88d266206
Akaikenlol <keithken972@gmail.com>
2023-10-23 00:14:47 +0630

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,30 @@ async function getOperator() {
 		"https://rhodesapi.up.railway.app/api/operator/ling"
 	);
 	const data = await response.json();
-	data.tags = data.tags.split("/");
+
+	if (typeof data.tags && data.class === "string") {
+		data.tags = data.tags.split("/");
+		data.class = data.class.split("/");
+	} else {
+		console.log("Data tags and class is not a string");
+	}
 	return data;
 }
 
 export default async function Home() {
 	const operator = await getOperator();
 	console.log("Operator: ", operator);
+
 	return (
 		<div className="text-center mt-10">
 			<h1 className="text-5xl font-bold">Home</h1>
 			<div className="flex flex-col gap-2 mt-5">
-				<p>{operator.name}</p>
-				<p>{operator.rarity}</p>
-				<p>{operator.class}</p>
-				<p>{operator.tags.join("/")}</p>
-				<p>{operator.traits}</p>
-				<p>{operator.availability}</p>
+				<p>Operator Name: {operator.name}</p>
+				<p>Rarity: {operator.rarity} ⭐</p>
+				<p>Class: {operator.class.join(" / ")}</p>
+				<p>Tags: {operator.tags.join(" / ")}</p>
+				<p>Traits: {operator.traits}</p>
+				<p>Availability: {operator.availability}</p>
 			</div>
 		</div>
 	);
